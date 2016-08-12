1428d317fb68ca17de300877c9fb96ab728edfe7
jyf <inernoro@163.com>
2016-08-12 11:50:24 +0800

    Fix bug

--- Cloud.Web/Controllers/AccountController.cs
+++ Cloud.Web/Controllers/AccountController.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 using Cloud.Web.Framework;
-using Microsoft.AspNet.Identity;
-using Microsoft.Owin.Security;
 
 namespace Cloud.Web.Controllers
 {
