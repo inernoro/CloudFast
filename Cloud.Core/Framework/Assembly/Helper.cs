465fd5b03b9c61b2d073787d656c59b69f95545d
jyf <inernoro@163.com>
2016-08-19 13:47:35 +0800

    update assembly and delete file

---
+++ Cloud.Core/Framework/Assembly/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Abp.Dependency;
+using Abp.Domain.Entities;
+using Abp.Runtime.Session;
+using Abp.UI;
+using Cloud.Domain;
+using Cloud.Framework.Redis;
+using Jil;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cloud.Framework.Assembly
+{
+    public static class Helper
+    {
+        #region Core
+
+        public static bool SignIn()
+        {
+            var session = IocManager.Instance.Resolve<IAbpSession>();
+            return session.UserId != null;
+        }
+
+        public static string Serialize<T>(this T t)
+        {
+            return JSON.Serialize(t);
+        }
+
+        public static T Deserialize<T>(this string str)
+        {
+            return JSON.Deserialize<T>(str);
+        }
+
+        #endregion
+    }
+}
--- Cloud.Core/Framework/Redis/IQueryHub.cs
+++
@@ -1,9 +0,0 @@
-﻿using Abp.Dependency;
-
-namespace Cloud.Framework.Redis
-{
-    public interface IQueryHub : ISingletonDependency
-    {
-        void Send(string groupName, string message);
-    }
-}
